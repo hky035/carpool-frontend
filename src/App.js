b9d11386a795370856f09dc652e9e527fabeae6c
hky035 <rldud035@naver.com>
2024-05-30 01:02:57 +0900

    feat: 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route path="" element={<Main />} />
+            <Route path="carpool" element={<Carpool />} />
+            <Route path="post" element={<Post />} />
+            <Route path="mileage" element={<Mileage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
