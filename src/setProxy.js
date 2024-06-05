91cf501588a466e2ed9123eceec683bc65953f17
hky035 <rldud035@naver.com>
2024-06-05 20:57:10 +0900

    feat: proxy 설정

--- src/setProxy.js
+++ src/setProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    
-    createProxyMiddleware('/api',{
-      target: 'http://localhost:8080/',
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
       changeOrigin: true,
     })
     );
