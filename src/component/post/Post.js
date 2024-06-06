779d9b35c08fdc9f01b2f1d85628f18e2c4673df
hky035 <rldud035@naver.com>
2024-06-06 12:30:28 +0900

    feat: 건의사항 작성 버튼 추가

--- src/component/post/Post.js
+++ src/component/post/Post.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import styled from 'styled-components';
 import PostCard from './PostCard';
+import {ReactComponent as RightArrow} from '../../style/asset/arrow-right-solid.svg'
+import { useNavigate } from 'react-router-dom';
 
 const Post = () => {
+    const navigate = useNavigate();
   return (
     <Wrapper>
         <Instroduction>
             <Title>건의 사항</Title>
-            <Description>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi voluptates sunt, impedit voluptatem sit suscipit enim, modi sequi quasi non dicta, debitis iusto iste ullam dolore autem saepe explicabo iure.</Description>
+              <DescContainer>
+                <Description>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi voluptates sunt, impedit voluptatem sit suscipit enim, modi sequi quasi non dicta, debitis iusto iste ullam dolore autem saepe explicabo iure.</Description>
+                  <GoToAddPost>
+                    <Label onClick={()=> navigate('/post/add')}>의견 남기기</Label>  
+                    <RightArrow/>
+                </GoToAddPost>
+            </DescContainer>
         </Instroduction>
 
         <PostContainer>
@@ -29,6 +38,33 @@ const Title = styled.h1`
     font-size : 35px;
     font-weight : 800;
     font-style : italic;
+`;
+
+const DescContainer = styled.div`
+    display : flex;
+    justify-content : space-between;
+    align-items : center;
+`;
+
+const Label = styled.p`
+    font-weight : bold;
+    font-style : italic;
+`;
+
+const GoToAddPost = styled.div`
+
+    height : 20px;
+    padding : 10px;
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    gap : 10px;
+    border-radius : 5px;
+    transition : 0.3s;
+
+    &:hover{
+        background-color : #fff;
+    }
 `;
 
 const Description = styled.p`
