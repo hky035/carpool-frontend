64dff4c9e131834b163c3aadedc1d3bd254b5626
hky035 <rldud035@naver.com>
2024-06-13 03:46:00 +0900

    feat: 회원가입 로직 수정

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,60 +1,67 @@
-import axios from 'axios';
-import React, { createContext, useContext, useEffect, useState } from 'react'
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    
-    const [isLogined, setIsLogined] = useState(false);
-    const [id, setId] = useState("");
-    const [studentNumber, setStudentNumber] = useState(0);
+  const [isLogined, setIsLogined] = useState(false);
+  const [id, setId] = useState("");
+  const [studentNumber, setStudentNumber] = useState(0);
 
-    useEffect(() => {
-        const sUser = JSON.parse(localStorage.getItem('user'));
-        if (sUser) {
-            setId(sUser);
-        }
-    }, []);
-
-    async function register(userId, password, studentNumber) {
-        try {
-            const res = await axios.post('/api/register', { userId, password, studentNumber });
-            return res.data;
-        } catch (error) {
-            console.error(error)
-        }
+  useEffect(() => {
+    const sUser = JSON.parse(localStorage.getItem("user"));
+    if (sUser) {
+      setId(sUser);
     }
+  }, []);
 
-    async function login(userId, password) {
-        try{
-            const res = await axios.post('/api/login', { userId, password });
+  async function register(userId, password, studentNumber) {
+    try {
+      const res = await axios.post("/api/register", {
+        userId,
+        password,
+        studentNumber,
+      });
 
-            if (res.data.userId === userId) {
-                setIsLogined(true);
-                setId(res.data.id);
-                console.log(res.data.id);
-                localStorage.setItem('user', JSON.stringify(res.data.id));
-                setStudentNumber(res.data.studentNumber);
-                return true;
-                }
-        } catch (error) {
-            console.error(error);
-        }
-
+      if (res.data.userId === userId) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    function logout() {
-        localStorage.removeItem('user');
-        setIsLogined(false);
+  async function login(userId, password) {
+    try {
+      const res = await axios.post("/api/login", { userId, password });
+
+      if (res.data.userId === userId) {
+        setIsLogined(true);
+        setId(res.data.id);
+        console.log(res.data.id);
+        localStorage.setItem("user", JSON.stringify(res.data.id));
+        setStudentNumber(res.data.studentNumber);
+        return true;
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  function logout() {
+    localStorage.removeItem("user");
+    setIsLogined(false);
+  }
 
   return (
-      <AuthContext.Provider value={{ isLogined, login, logout, register, id}}>
-          {children}
+    <AuthContext.Provider value={{ isLogined, login, logout, register, id }}>
+      {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
