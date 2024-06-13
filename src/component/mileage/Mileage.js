4baddbfa22f7301411ca215cd9e819cc94415a12
hky035 <rldud035@naver.com>
2024-06-13 14:00:18 +0900

    refactor: 리팩토링

--- src/component/mileage/Mileage.js
+++ src/component/mileage/Mileage.js
@@ -1,75 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components';
-import MileageCard from './MileageCard';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import MileageCard from "./MileageCard";
+import apiClient from "../../api/apiClient";
 
 const Mileage = () => {
+  const [mileageItem, setMileageItem] = useState([]);
 
-    const [mileageItem, setMileageItem] = useState([]);
-
-    useEffect(() => {
-        const fetchMileageItems = async () => {
-            const res = await axios.get('/api/mileage-items');
-            console.log(res.data);
-            setMileageItem(res.data);
-        }
-        fetchMileageItems();
-     }, [])
+  useEffect(() => {
+    const fetchMileageItems = async () => {
+      const res = await apiClient.get("/api/mileage-items");
+      setMileageItem(res.data);
+    };
+    fetchMileageItems();
+  }, []);
 
   return (
     <Wrapper>
+      <Instroduction>
+        <Title>마일리지 샵</Title>
+        <Description>
+          획득한 마일리지로 다양한 교내 서비스 상품을 구매해보세요!
+          <br />
+          원하시는 상품이 있다면 '구매' 버튼을 눌러 구매하세요.
+          <br />
+          원하시는 교내 서비스가 있다면 건의사항 게시판에 남겨주세요!
+        </Description>
+      </Instroduction>
 
-        <Instroduction>
-            <Title>마일리지 샵</Title>
-              <Description>
-                  획득한 마일리지로 다양한 교내 서비스 상품을 구매해보세요!
-                  <br />
-                  원하시는 상품이 있다면 '구매' 버튼을 눌러 구매하세요.
-                  <br />
-                  원하시는 교내 서비스가 있다면 건의사항 게시판에 남겨주세요!
-              </Description>
-        </Instroduction>
-
-        <ItemContainer>
-            {
-                  mileageItem.map(item => {
-                    console.log("item", item)
-                    return <MileageCard item={item}></MileageCard>
-                })
-            }
-        </ItemContainer>
-
+      <ItemContainer>
+        {mileageItem.map((item) => {
+          return <MileageCard key={item.id} item={item}></MileageCard>;
+        })}
+      </ItemContainer>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
-    box-sizing : border-box;
-    padding : 0 80px;
+  box-sizing: border-box;
+  padding: 0 80px;
 `;
 
 const Title = styled.h1`
-    font-size : 35px;
-    font-weight : 800;
-    font-style : italic;
-    margin-bottom : 15px;
+  font-size: 35px;
+  font-weight: 800;
+  font-style: italic;
+  margin-bottom: 15px;
 `;
 
 const Description = styled.p`
-    margin : 10px 0;
-    width : 600px;
-    line-height : 25px;
+  margin: 10px 0;
+  width: 600px;
+  line-height: 25px;
 `;
 
 const Instroduction = styled.div`
-    margin : 30px 0;
+  margin: 30px 0;
 `;
 
 const ItemContainer = styled.div`
-    display : flex;
-    justify-content : space-between;
-    flex-wrap : wrap;
-    margin : 30px 0;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 30px 0;
 `;
 
-export default Mileage
+export default Mileage;
--- src/component/mileage/MileageCard.js
+++ src/component/mileage/MileageCard.js
@@ -1,92 +1,131 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import apiClient from "../../api/apiClient";
+import { useAuth } from "../../AuthProvider";
+import { ReactComponent as Coin } from "../../style/asset/coins-solid.svg";
+import { useNavigate } from "react-router-dom";
 
 const MileageCard = ({ item }) => {
-
-    const imgPath = (title) => {
-        switch (title) {
-            case "학생식당 식사 구매권":
-                console.log('excuted!!');
-                return "assets/meal-coupon.png";
-            case "도서관 예약권":
-                return "assets/booking-seat-coupon.png";
-            case "주차할인권":
-                return "assets/parkingfee-discount-coupon.png";
-            case "도서대출 연장권":
-                return "assets/book-loan-extension-coupon.png";
-            default:
-                return null;
-        }
+  const imgPath = (title) => {
+    switch (title) {
+      case "학생식당 식사 구매권":
+        return "assets/meal-coupon.png";
+      case "도서관 예약권":
+        return "assets/booking-seat-coupon.png";
+      case "주차할인권":
+        return "assets/parkingfee-discount-coupon.png";
+      case "도서대출 연장권":
+        return "assets/book-loan-extension-coupon.png";
+      default:
+        return null;
     }
+  };
+
+  const authContext = useAuth();
+  const navigate = useNavigate();
+
+  const buyItem = (id) => {
+    if (!authContext.isLogined) {
+      navigate("/login");
+      return;
+    }
+
+    const buy = async () => {
+      const result = await apiClient.post("/api/mileage-item/buy", {
+        userId: authContext.id,
+        itemId: id,
+      });
+
+      if (result.data.change >= 0) {
+        alert(
+          `${result.data.userId}님, ${result.data.title} 구매 완료하였습니다. \n잔액 : ${result.data.change}`
+        );
+      } else {
+        alert(
+          `${result.data.userId}님, 마일리지가 부족하여 ${result.data.title}를 구매하지 못 하였습니다.}`
+        );
+      }
+    };
+    buy();
+  };
 
   return (
     <Wrapper>
-          <Img src={imgPath(item.title)}></Img>
-        <Description>
-            <Title>{item.title}</Title>
-              <Body>{item.description}</Body>
-            <BuyBtn>구매</BuyBtn>
-        </Description>
+      <Img src={imgPath(item.title)}></Img>
+      <Description>
+        <Title>{item.title}</Title>
+        <Body>{item.description}</Body>
+        <Price>
+          <Coin />
+          {item.price}
+        </Price>
+        <BuyBtn onClick={() => buyItem(item.id)}>구매</BuyBtn>
+      </Description>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
-    width : 48%;
-    display : flex;
-    justify-content : space-between;
-    margin-bottom : 20px;
-    align-items : center;
-    
-
+  width: 48%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  align-items: center;
 `;
 
 const Img = styled.img`
-    width : 35%;
-    height : 55%;
-    margin : 0 auto;
+  width: 35%;
+  height: 55%;
+  margin: 0 auto;
 `;
 
 const Description = styled.div`
-    width : 60%;
-    background-color : #fff;
-    box-shadow : var(--shadow);
-    border-radius : 15px;
-    box-sizing : border-box;
-    padding : 20px 15px 10px;
+  width: 60%;
+  background-color: #fff;
+  box-shadow: var(--shadow);
+  border-radius: 15px;
+  box-sizing: border-box;
+  padding: 20px 15px 10px;
 `;
 
 const Title = styled.p`
-    font-size : 20px;
-    font-weight : bold;
-    margin-bottom : 10px;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
 `;
 
 const Body = styled.p`
-    margin-bottom : 15px;
-    height : 65px;
+  height: 65px;
 
-    overflow-y : hidden;
-`
-
-const BuyBtn = styled.div`
-    box-sizing : 10px 20px;
-    font-size : 15px;
-    font-weight : bold;
-    background-color : #64CD3C;
-
-    padding : 10px 20px;
-    width : 50px;
-    text-align : center;
-    border-radius : 5px;
-
-    transition : 0.3s;
-    cursor: pointer;
-
-    &:hover{
-        /* background-color : rgba(0,150,0,0.8); */
-        color : #fff;
-    }
+  overflow-y: hidden;
 `;
 
-export default MileageCard
+const Price = styled.div`
+  font-size: 17px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  display: flex;
+  gap: 8px;
+`;
+
+const BuyBtn = styled.div`
+  box-sizing: 10px 20px;
+  font-size: 15px;
+  font-weight: bold;
+  background-color: #64cd3c;
+
+  padding: 10px 20px;
+  width: 50px;
+  text-align: center;
+  border-radius: 5px;
+
+  transition: 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    /* background-color : rgba(0,150,0,0.8); */
+    color: #fff;
+  }
+`;
+
+export default MileageCard;
